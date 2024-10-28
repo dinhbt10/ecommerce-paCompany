9b34b10676391dffd6ed61bf27ac2ed246b5d739
dinhbt <dinhbt.1110.work@gmail.com>
2024-10-28 10:50:52 +0700

    Update

---
+++ src/pages/admins/Dashboard/Config.js
@@ -0,0 +1,53 @@
+import { formatNumber } from "../../../utils/common";
+
+export const optionChart = {
+  series: [],
+  chart: {
+    type: "bar",
+    height: 350,
+    toolbar: {
+      show: false,
+    },
+  },
+  plotOptions: {
+    bar: {
+      horizontal: false,
+      columnWidth: "55%",
+      endingShape: "rounded",
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    show: true,
+    width: 2,
+    colors: ["transparent"],
+  },
+  fill: {
+    opacity: 1,
+  },
+  xaxis: {
+    categories: [
+      "Tháng 1",
+      "Tháng 2",
+      "Tháng 3",
+      "Tháng 4",
+      "Tháng 5",
+      "Tháng 6",
+      "Tháng 7",
+      "Tháng 8",
+      "Tháng 9",
+      "Tháng 10",
+      "Tháng 11",
+      "Tháng 12",
+    ],
+  },
+  yaxis: {
+    min: 0,
+    tickAmount: 5,
+    labels: {
+      formatter: (value) => `${formatNumber(value)}`,
+    },
+  },
+};
--- src/pages/admins/Dashboard/Dashboard.jsx
+++ src/pages/admins/Dashboard/Dashboard.jsx
@@ -1,16 +1,32 @@
 import { useEffect, useState } from "react";
 import instance from "../../../utils/http";
 import { Table } from "flowbite-react";
-import { formatNumber } from "../../../utils/common";
+import { convertDate, formatNumber } from "../../../utils/common";
+import Chart from "react-apexcharts";
+import { optionChart } from "./Config";
 
 const Dashboard = () => {
   const [topBookSale, setTopBookSale] = useState([]);
+  const [orders, setOrders] = useState([]);
+  const [charts, setCharts] = useState(optionChart);
+
   const getTopProductSale = async () => {
     const res = await instance.get("book/books-sales");
-    const { data, success } = res.data;
-    if (success) {
-      setTopBookSale(data);
-    }
+    const resAPIOrder = await instance.get("orders/list/admin");
+    const resAPIChart = await instance.get("orders/revenue-chart");
+    Promise.all([res, resAPIOrder, resAPIChart]).then((r) => {
+      setTopBookSale(r[0].data.data);
+      setOrders(r[1].data.data.orders);
+      setCharts((prev) => ({
+        ...prev,
+        series: [
+          {
+            name: "Doanh thu",
+            data: r[2].data.data.data,
+          },
+        ],
+      }));
+    });
   };
 
   useEffect(() => {
@@ -19,53 +35,95 @@ const Dashboard = () => {
 
   return (
     <div>
-      <div className="bg-white rounded p-4">
-        <div className="text-xl">Top sản phẩm bán chạy</div>
-        <Table border className="mt-3">
-          <Table.Body className="divide-y border">
-            {topBookSale.length > 0 &&
-              topBookSale?.map((item, index) => (
-                <Table.Row
-                  key={index}
-                  className="bg-white dark:border-gray-700 dark:bg-gray-800"
-                >
-                  <Table.Cell>
-                    <div className="flex flex-row items-center gap-2">
-                      <div className="bg-black h-[80px] min-w-[80px]">
-                        <img
-                          src={item.imageUrls[0]}
-                          alt="anh"
-                          className="h-full w-full object-contain"
-                        />
+      <div className="bg-white mb-5 p-6 rounded">
+        <div className="text-xl font-medium">Doanh thu năm hiện tại</div>
+        <Chart
+          series={charts.series}
+          options={charts}
+          type="bar"
+          height={350}
+        />
+      </div>
+      <div className="grid grid-cols-2 gap-2">
+        <div className="col-span-1">
+          <div className="bg-white rounded p-6">
+            <div className="text-xl font-medium">Giao dịch gần đây</div>
+            <Table className="mt-3">
+              <Table.Head className="border-b">
+                <Table.HeadCell>Khác hàng</Table.HeadCell>
+                <Table.HeadCell>Ngày đặt hàng</Table.HeadCell>
+                <Table.HeadCell>Số lượng</Table.HeadCell>
+                <Table.HeadCell>Trạng thái</Table.HeadCell>
+              </Table.Head>
+              <Table.Body className="">
+                {orders.length > 0 &&
+                  orders.map((item, index) => {
+                    if (index < 5) {
+                      return (
+                        <Table.Row key={index} className="bg-white border-b">
+                          <Table.Cell>{item.receiveName}</Table.Cell>
+                          <Table.Cell>{convertDate(item.createdAt)}</Table.Cell>
+                          <Table.Cell>{item.books.length}</Table.Cell>
+                          <Table.Cell>{item.status}</Table.Cell>
+                        </Table.Row>
+                      );
+                    }
+                  })}
+                {orders.length === 0 && (
+                  <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                    <Table.Cell colSpan={6}>
+                      <div className="flex justify-center items-center h-[300px]">
+                        Không có dữ liệu
                       </div>
-                      <div className="">{item.bookName}</div>
-                    </div>
-                  </Table.Cell>
-                  <Table.Cell>
-                    <div className="flex flex-col items-center gap-2">
-                      Giá tiền
-                      <div className="">{formatNumber(item.bookPrice)}</div>
-                    </div>
-                  </Table.Cell>
-                  <Table.Cell>
-                    <div className="flex flex-col items-center gap-2">
-                      Lượt bán
-                      <div className="">{item.totalSold}</div>
-                    </div>
-                  </Table.Cell>
-                </Table.Row>
-              ))}
-            {topBookSale.length === 0 && (
-              <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                <Table.Cell colSpan={6}>
-                  <div className="flex justify-center items-center h-[300px]">
-                    Không có dữ liệu
-                  </div>
-                </Table.Cell>
-              </Table.Row>
-            )}
-          </Table.Body>
-        </Table>
+                    </Table.Cell>
+                  </Table.Row>
+                )}
+              </Table.Body>
+            </Table>
+          </div>
+        </div>
+        <div className="col-span-1">
+          <div className="bg-white rounded p-6">
+            <div className="text-xl font-medium">Top sản phẩm bán chạy</div>
+            <Table className="mt-3">
+              <Table.Head className="border-b">
+                <Table.HeadCell>Tên sản phẩm</Table.HeadCell>
+                <Table.HeadCell>Giá</Table.HeadCell>
+                <Table.HeadCell>Lượt bán</Table.HeadCell>
+              </Table.Head>
+              <Table.Body className="">
+                {topBookSale.length > 0 &&
+                  topBookSale?.map((item, index) => (
+                    <Table.Row key={index} className="bg-white border-b">
+                      <Table.Cell>
+                        <div className="flex flex-row items-center gap-2">
+                          <div className="bg-black h-[20px] min-w-[20px]">
+                            <img
+                              src={item.imageUrls[0]}
+                              alt="anh"
+                              className="h-full w-full object-contain"
+                            />
+                          </div>
+                          <div className="text-[13px]">{item.bookName}</div>
+                        </div>
+                      </Table.Cell>
+                      <Table.Cell>{formatNumber(item.bookPrice)}</Table.Cell>
+                      <Table.Cell>{item.totalSold}</Table.Cell>
+                    </Table.Row>
+                  ))}
+                {topBookSale.length === 0 && (
+                  <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                    <Table.Cell colSpan={6}>
+                      <div className="flex justify-center items-center h-[300px]">
+                        Không có dữ liệu
+                      </div>
+                    </Table.Cell>
+                  </Table.Row>
+                )}
+              </Table.Body>
+            </Table>
+          </div>
+        </div>
       </div>
     </div>
   );
